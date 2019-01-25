5c9ac5023ec36177dbdf0dd13592cd3bd56a925e
NhatDinh <nhat.ldinh@gmail.com>
2019-01-25 06:52:11 -0800

    reposition components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,18 +17,16 @@ const Footer = props => (
 				<p style={divStyle}>
 					Having to do grocery shopping before, I repeatedly went through
 					the {"\n"}
-					tiresome process of finding the right ingredients, marking
-					it out of {"\n"}
-					shopping list, and still forgetting stuff. That's why I
+					tiresome process of finding the right ingredients, making notes and marking
+					it off of {"\n"}
+					my notes, and still manage to forget stuff. That's why I
 					decided to {"\n"}
-					build Novice Chef, a To-do/ Ingredients app.
+					build Novice Chef, an Ingredients Search and Checklist app.
 				</p>
 			</div>
 			<div className="col-md tribute" style={divStyle} >
-			<h5> Powered by <i className="fas fa-tape"> Food2Fork API </i></h5>
-			<h5>Stack: <i className="fas fa-plug"> ReactJS,  ExpressJS,  MongoDB, NodeJS </i></h5>
-			<h5> Animation by <i className="fab fa-dribbble-square"> Dribble </i></h5>
-		
+			<h5> Powered by <i className="fas fa-tape"> Food2Fork API and Edamam API </i></h5>
+			<h5> Stack: <i className="fas fa-plug"> ReactJS, NodeJS, a dash of Bootstrap and lots of CSS tricks </i></h5>
 			</div>
 			</div>
 		</div>
